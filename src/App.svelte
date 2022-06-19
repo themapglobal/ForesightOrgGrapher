39b0aed48439f7d5b89cdab786db756ffe94e1d2
Nilesh <19304+nileshtrivedi@users.noreply.github.com>
2022-06-19 09:49:16 +0100

    tag groups and switch to sl-select

--- src/App.svelte
+++ src/App.svelte
@@ -9,7 +9,7 @@
             "label": "Arithmetic",
             "desc": "",
             "link": "",
-            "tags": ["foo"],
+            "tags": ["org:meta"],
             "parent": null,
             "pos": {
                 "x": 264,
@@ -22,7 +22,7 @@
             "label": "Algebra",
             "desc": "",
             "link": "",
-            "tags": ["foo", "bar"],
+            "tags": ["org:google", "lab:cambridge"],
             "parent": null,
             "pos": {
                 "x": 155,
@@ -35,7 +35,7 @@
             "label": "is a prerequisite of",
             "desc": "",
             "link": "",
-            "tags": ["bar","x"],
+            "tags": ["org:apple","lab:mit"],
             "directed": true,
             "weight": 5,
             "fromId": 3,
@@ -47,7 +47,7 @@
             "label": "",
             "desc": "",
             "link": "",
-            "tags": ["baz","foo"],
+            "tags": ["org:apple","org:google"],
             "directed": true,
             "weight": 5,
             "fromId": 3,
@@ -72,7 +72,7 @@
             "label": "Multiplication",
             "desc": "",
             "link": "",
-            "tags": [],
+            "tags": ["org:apple","lab:mit"],
             "parent": 2,
             "pos": {
                 "x": 70,
@@ -85,7 +85,7 @@
             "label": "Quadratic Equations",
             "desc": "",
             "link": "",
-            "tags": [],
+            "tags": ["org:apple","org:google"],
             "parent": 1,
             "pos": {
                 "x": 150,
@@ -187,30 +187,49 @@
         };
 
     let highlighted = [];
+
+    function getTagGroups(graph){
+        // get unique tags
+        let tags = graph.items.map(i => i.tags).flat().filter((value, index, self) => self.indexOf(value) === index); 
+        let groups = tags.reduce((acc, tag)=> {
+            let [kind,name] = tag.split(":");
+            acc[kind] = acc[kind] ?? [];
+            acc[kind].push(name);
+            return acc;
+        }, {})
+        console.log(groups);
+        return Object.entries(groups);
+    }
 </script>
 
 <MapEditor unsizedGraph={graph} {highlighted} on:graphchanged="{e => {graph = e.detail;}}"/>
 
 <section>
-<sl-dropdown 
-    on:sl-select={e => highlighted = graph.items.filter(i => i.kind === 'node' && i.tags.includes(e.detail.item.value)).map(i => i.id)}>
-    <sl-button slot="trigger" caret>Highlight by Tag</sl-button>
-    <sl-menu>
-        {#each graph.items.map(i => i.tags).flat().filter((value, index, self) => self.indexOf(value) === index) as tag}
-        <sl-menu-item value={tag}>{tag}</sl-menu-item>
+<sl-select 
+    placeholder="Highlight by Tag"
+    on:sl-change={e => highlighted = graph.items.filter(i => i.kind === 'node' && i.tags.includes(e.target.value)).map(i => i.id)}
+>
+        {#each getTagGroups(graph) as group}
+            <sl-menu-label>{group[0]}</sl-menu-label>
+            {#each group[1] as tag}
+                <sl-menu-item value={`${group[0]}:${tag}`}>{tag}</sl-menu-item>
+            {/each}
+            <sl-divider></sl-divider>
         {/each}
-    </sl-menu>
-</sl-dropdown>
+</sl-select>
 </section>
 
 <style>
     section {
-        display: none;
         position: fixed;
         top: 20px;
         left: 20px;
         background-color: orange;
         border-radius: 3px;
         border: 2px solid red;
+    }
+
+    sl-select {
+        width: 200px;
     }
 </style>
